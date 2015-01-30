e6de35ad0748c47bf03e2c0bb525be26a8cc31a2
rodrik <rodrigo.ijk@gmail.com>
2015-01-29 23:33:03 -0200

    Finished lab

--- PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -78,7 +78,8 @@ public class BookmarksActivity extends Activity {
 
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
-		// TODO - Start the GoToDangerousActivity
+		// Start the GoToDangerousActivity
+		startActivity(new Intent("course.labs.permissions.DANGEROUS_ACTIVITY"));
 		
 
 	}
