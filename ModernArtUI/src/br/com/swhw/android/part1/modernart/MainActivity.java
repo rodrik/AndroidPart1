4b1d5749364e6712090030fc5b2e87debcc30bc9
rodrik <rodrigo.ijk@gmail.com>
2015-02-11 00:09:16 -0200

    Project finished

--- ModernArtUI/src/br/com/swhw/android/part1/modernart/MainActivity.java
+++ ModernArtUI/src/br/com/swhw/android/part1/modernart/MainActivity.java
@@ -1,33 +1,48 @@
 package br.com.swhw.android.part1.modernart;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private final static String TAG = "ModerArtUI-app";
 	private SeekBar seekBar;
-
+	
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	private Button button5;
+	
+	private int color1;
+	private int color2;
+	private int color3;
+	private int color5;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
+
+		color1 = getResources().getColor(R.color.button1);
+		color2 = getResources().getColor(R.color.button2);
+		color3 = getResources().getColor(R.color.button3);
+		color5 = getResources().getColor(R.color.button5);
+		Log.d(TAG, "Default Red: " + Color.red(color1));
+		
+		button1 = (Button)findViewById(R.id.button1);
+		button2 = (Button)findViewById(R.id.button2);
+		button3 = (Button)findViewById(R.id.button3);
+		button5 = (Button)findViewById(R.id.button5);
 		
 		seekBar = (SeekBar) findViewById(R.id.seekBar1);
 		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			int progressChanged = 0;
-			
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
@@ -38,12 +53,11 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				progressChanged = progress;
-				Toast.makeText(MainActivity.this,"seek bar progress:"+progressChanged, Toast.LENGTH_SHORT).show();
+				changeButtonsColor(progress);
 			}
 		});
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -53,16 +67,23 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-
 	    int id = item.getItemId();
-
 	    if (id == R.id.action_moreinformation) {
-	        Log.d(TAG, "Clicou aqui!");
 	        new VisitMomaDialogFragment().show(getFragmentManager(), "MomaDialog");
-
+	        
 	    }
-
 	    return true;
-
+	}
+	
+	private void changeButtonsColor(int seekbarValue) {
+		button1.setBackgroundColor(Color.rgb(getNewValue(Color.red(color1), seekbarValue), getNewValue(Color.green(color1), seekbarValue), getNewValue(Color.blue(color1), seekbarValue)));
+		button2.setBackgroundColor(Color.rgb(getNewValue(Color.red(color2), seekbarValue), Color.green(color2), getNewValue(Color.blue(color2), seekbarValue)));
+		button3.setBackgroundColor(Color.rgb(getNewValue(Color.red(color3), seekbarValue), getNewValue(Color.green(color3), seekbarValue), Color.blue(color3)));
+		button5.setBackgroundColor(Color.rgb(getNewValue(Color.red(color5), seekbarValue), getNewValue(Color.green(color5), seekbarValue), getNewValue(Color.blue(color5), seekbarValue)));
+	}
+	
+	private int getNewValue(int defaultValue, int rate) {
+		double step = (175 - defaultValue)/100d;
+		return (int)(defaultValue+(step/2 * rate));
 	}
 }
